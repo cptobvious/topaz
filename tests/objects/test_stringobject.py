28d476110f10045893a2bcb3b65fe560dd9c3954
Chris Blättermann <blaettermann@softyou.de>
2012-06-25 00:28:03 +0200

    updated tests for strings

--- tests/objects/test_stringobject.py
+++ tests/objects/test_stringobject.py
@@ -4,31 +4,31 @@ from ..base import BaseRuPyPyTest
 class TestStringObject(BaseRuPyPyTest):
     def test_lshift(self, space):
         w_res = space.execute('return "abc" << "def" << "ghi"')
-        assert space.str_w(w_res) == "abcdefghi"
+        assert self.unwrap(space, w_res) == "abcdefghi"
 
     def test_plus(self, space):
         w_res = space.execute('return "abc" + "def" + "ghi"')
-        assert space.str_w(w_res) == "abcdefghi"
+        assert self.unwrap(space, w_res) == "abcdefghi"
 
     def test_to_s(self, space):
         w_res = space.execute('return "ABC".to_s')
-        assert space.str_w(w_res) == "ABC"
+        assert self.unwrap(space, w_res) == "ABC"
 
     def test_length(self, space):
         w_res = space.execute("return 'ABC'.length")
-        assert space.int_w(w_res) == 3
+        assert self.unwrap(space, w_res) == 3
 
     def test_comparator_lt(self, space):
         w_res = space.execute("return 'a' <=> 'b'")
-        assert space.int_w(w_res) == -1
+        assert self.unwrap(space, w_res) == -1
 
     def test_comparator_eq(self, space):
         w_res = space.execute("return 'a' <=> 'a'")
-        assert space.int_w(w_res) == 0
+        assert self.unwrap(space, w_res) == 0
 
     def test_comparator_gt(self, space):
         w_res = space.execute("return 'b' <=> 'a'")
-        assert space.int_w(w_res) == 1
+        assert self.unwrap(space, w_res) == 1
 
     def test_hash(self, space):
         w_res = space.execute("""
@@ -55,15 +55,15 @@ class TestStringObject(BaseRuPyPyTest):
 
     def test_subscript(self, space):
         w_res = space.execute("return 'abcdefg'[1]")
-        assert space.str_w(w_res) == "b"
+        assert self.unwrap(space, w_res) == "b"
 
     def test_range_inclusive(self, space):
         w_res = space.execute("return 'abcdefg'[1..2]")
-        assert space.str_w(w_res) == "bc"
+        assert self.unwrap(space, w_res) == "bc"
 
     def test_range_exclusive(self, space):
         w_res = space.execute("return 'abcdefg'[1...3]")
-        assert space.str_w(w_res) == "bc"
+        assert self.unwrap(space, w_res) == "bc"
 
     def test_edge_indices(self, space):
         w_res = space.execute("return 'hello'[5]")
@@ -91,7 +91,7 @@ class TestStringObject(BaseRuPyPyTest):
         assert self.unwrap(space, w_res) == "lo"
 
         w_res = space.execute("return 'hello'[4..2]")
-        assert space.str_w(w_res) == ""
+        assert self.unwrap(space, w_res) == ""
 
         w_res = space.execute("return 'hello'[8..10]")
         assert self.unwrap(space, w_res) is None
@@ -112,4 +112,3 @@ class TestStringObject(BaseRuPyPyTest):
     def test_object_id(self, space):
         w_res = space.execute("return 'asd'.object_id")
         assert self.unwrap(space, w_res) >= 0
->>>>>>> more methods for stringobject
