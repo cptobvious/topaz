b99f2f085f0f26ffa65d06a57cf606181b28541d
Chris Blättermann <blaettermann@softyou.de>
2012-06-24 20:36:45 +0200

    more methods for stringobject

--- tests/objects/test_stringobject.py
+++ tests/objects/test_stringobject.py
@@ -52,3 +52,64 @@ class TestStringObject(BaseRuPyPyTest):
 
         w_res = space.execute("return ('a' << 'b').clear")
         assert self.unwrap(space, w_res) == ""
+
+    def test_subscript(self, space):
+        w_res = space.execute("return 'abcdefg'[1]")
+        assert space.str_w(w_res) == "b"
+
+    def test_range_inclusive(self, space):
+        w_res = space.execute("return 'abcdefg'[1..2]")
+        assert space.str_w(w_res) == "bc"
+
+    def test_range_exclusive(self, space):
+        w_res = space.execute("return 'abcdefg'[1...3]")
+        assert space.str_w(w_res) == "bc"
+
+    def test_edge_indices(self, space):
+        w_res = space.execute("return 'hello'[5]")
+        assert self.unwrap(space, w_res) == None
+
+        w_res = space.execute("return 'hello'[-2]")
+        assert self.unwrap(space, w_res) == "l"
+
+        w_res = space.execute("return 'hello'[-6]")
+        assert self.unwrap(space, w_res) == None
+
+        w_res = space.execute("return 'hello'[-2..0]")
+        assert self.unwrap(space, w_res) == ""
+
+        w_res = space.execute("return 'hello'[5..5]")
+        assert self.unwrap(space, w_res) == ""
+
+        w_res = space.execute("return 'hello'[5..8]")
+        assert self.unwrap(space, w_res) == ""
+
+        w_res = space.execute("return 'hello'[-3..-2]")
+        assert self.unwrap(space, w_res) == "ll"
+
+        w_res = space.execute("return 'hello'[-2..-1]")
+        assert self.unwrap(space, w_res) == "lo"
+
+        w_res = space.execute("return 'hello'[4..2]")
+        assert space.str_w(w_res) == ""
+
+        w_res = space.execute("return 'hello'[8..10]")
+        assert self.unwrap(space, w_res) == None
+
+        w_res = space.execute("return 'hello'[3..-2]")
+        assert self.unwrap(space, w_res) == "l"
+
+        w_res = space.execute("return 'hello'[-2..0]")
+        assert self.unwrap(space, w_res) == ""
+
+        w_res = space.execute("return 'hello'[-2...1]")
+        assert self.unwrap(space, w_res) == ""
+
+    def test_succ(self, space):
+        w_res = space.execute("return 'a'.succ")
+        assert self.unwrap(space, w_res) == "b"
+
+    def test_object_id(self, space):
+        w_res = space.execute("return 'asd'.object_id")
+        assert self.unwrap(space, w_res) >= 0
+>>>>>>> more methods for stringobject
