aee276f5e891b651d5af1e68b4a3fd92ff774949
Chris Blättermann <blaettermann@softyou.de>
2012-06-26 19:56:26 +0200

    implemented module comparison

--- tests/objects/test_objectobject.py
+++ tests/objects/test_objectobject.py
@@ -70,7 +70,6 @@ class TestObjectObject(BaseRuPyPyTest):
         w_res = space.execute("return [].is_a? Array")
         assert self.unwrap(space, w_res) is True
 
-    @py.test.mark.xfail
     def test_is_a_module(self, space):
         w_res = space.execute("return [].is_a? Enumerable")
         assert self.unwrap(space, w_res) is True
@@ -88,7 +87,6 @@ class TestObjectObject(BaseRuPyPyTest):
         w_res = space.execute("return [].instance_of? Array")
         assert self.unwrap(space, w_res) is True
 
-    @py.test.mark.xfail
     def test_instance_of_module(self, space):
         w_res = space.execute("return [].instance_of? Enumerable")
         assert self.unwrap(space, w_res) is False
