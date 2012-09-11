ed36eae0cfab3d4c747946ecb8e7a4a0b9923272
Chris Blättermann <blaettermann@softyou.de>
2012-09-11 15:36:45 +0200

    some functions for ranges

--- rupypy/objects/rangeobject.py
+++ rupypy/objects/rangeobject.py
@@ -7,11 +7,22 @@ class W_RangeObject(W_Object):
     classdef = ClassDef("Range", W_Object.classdef)
     classdef.include_module(Enumerable)
 
-    def __init__(self, space, w_start, w_end, exclusive):
+    def __init__(self, space, w_start, w_end, exclusive=False):
         W_Object.__init__(self, space)
         self.w_start = w_start
         self.w_end = w_end
         self.exclusive = exclusive
+
+    @classdef.singleton_method("allocate")
+    def method_allocate(self, space):
+        return W_RangeObject(space, space.w_nil, space.w_nil)
+
+    @classdef.singleton_method("new")
+    def method_new(self, space, w_start, w_end, w_exclusive=None):
+        if w_exclusive is None:
+            w_exclusive = space.newbool(False)
+        exclusive = w_exclusive.is_true(space)
+        return W_RangeObject(space, w_start, w_end, exclusive)
 
     @classdef.method("begin")
     def method_begin(self, space):
@@ -26,6 +37,69 @@ class W_RangeObject(W_Object):
         return space.newbool(self.exclusive)
 
     classdef.app_method("""
+    def ==(other)
+        if other.kind_of? Range
+            if self.begin == other.begin && self.end == other.end &&
+                if self.exclude_end? == other.exclude_end?
+                    return true
+                end
+            end
+        end
+        false
+    end
+
+    def ===(other)
+        include? other
+    end
+
+    def member?(other)
+        include? other
+    end
+
+    def include?(other)
+        if other.is_a? Fixnum
+            return cover? other
+        end
+
+        if self.begin.is_a?(String) && self.begin.length == 1
+            if self.end.is_a?(String) && self.end.length == 1
+                if other.is_a?(String) && other.length == 1
+                    if self.begin.ord <= other.ord
+                        if self.exclude_end?
+                            if other.ord < self.end.ord
+                                return true
+                            end
+                        else
+                            if other.ord <= self.end.ord
+                                return true
+                            end
+                        end
+                    end
+                end
+            end
+        end
+
+        self.each do |i|
+            return true if i == other
+        end
+        false
+    end
+
+    def cover?(other)
+        if self.begin <= other
+            if self.exclude_end?
+                if other < self.end
+                    return true
+                end
+            else
+                if other <= self.end
+                    return true
+                end
+            end
+        end
+        false
+    end
+
     def each
         i = self.begin
         lim = self.end
@@ -36,5 +110,13 @@ class W_RangeObject(W_Object):
             yield i
             i = i.succ
         end
+    end
+
+    def to_a
+        x = []
+        self.each do |elem|
+            x << elem
+        end
+        x
     end
     """)
--- tests/objects/test_rangeobject.py
+++ tests/objects/test_rangeobject.py
@@ -3,24 +3,133 @@ from ..base import BaseRuPyPyTest
 
 class TestRangeObject(BaseRuPyPyTest):
     def test_map(self, space):
-        w_res = space.execute("""
-        return (1..3).map do |x|
-            x * 5
-        end
-        """)
+        w_res = space.execute("return (1..3).map {|x| x * 5}")
         assert self.unwrap(space, w_res) == [5, 10, 15]
 
+        w_res = space.execute("return (1...3).map {|x| x * 5}")
+        assert self.unwrap(space, w_res) == [5, 10]
+
     def test_starting_point_always_returned(self, space):
-        w_res = space.execute("""
-        return (1..1).map do |x|
-            x
-        end
-        """)
+        w_res = space.execute("return (1..1).map {|x| x}")
         assert self.unwrap(space, w_res) == [1]
+
+    def test_to_a(self, space):
+        w_res = space.execute("return (1..3).to_a")
+        assert self.unwrap(space, w_res) == [1, 2, 3]
+
+        w_res = space.execute("return (1...3).to_a")
+        assert self.unwrap(space, w_res) == [1, 2]
+
+        w_res = space.execute("return (3..2).to_a")
+        assert self.unwrap(space, w_res) == []
+
+    def test_begin(self, space):
+        w_res = space.execute("return (5..10).begin")
+        assert self.unwrap(space, w_res) == 5
+
+        w_res = space.execute("return ('1'..'5').begin")
+        assert self.unwrap(space, w_res) == "1"
+
+    def test_end(self, space):
+        w_res = space.execute("return (5..10).end")
+        assert self.unwrap(space, w_res) == 10
+
+        w_res = space.execute("return (5...10).end")
+        assert self.unwrap(space, w_res) == 10
+
+        w_res = space.execute("return ('1'..'5').end")
+        assert self.unwrap(space, w_res) == "5"
+
+        w_res = space.execute("return ('1'...'5').end")
+        assert self.unwrap(space, w_res) == "5"
 
     def test_exclude_end(self, space):
         w_res = space.execute("return (1..5).exclude_end?")
         assert self.unwrap(space, w_res) is False
 
         w_res = space.execute("return (1...5).exclude_end?")
+        assert self.unwrap(space, w_res) is True
+
+    def test_eql(self, space):
+        w_res = space.execute("return (1..2) == (1..2)")
+        assert self.unwrap(space, w_res) is True
+
+        w_res = space.execute("return (1...2) == (1...2)")
+        assert self.unwrap(space, w_res) is True
+
+        w_res = space.execute("return (1..2) == 4")
+        assert self.unwrap(space, w_res) is False
+
+        w_res = space.execute("return (1..2) == Range.new(1, 2)")
+        assert self.unwrap(space, w_res) is True
+
+        w_res = space.execute("return (1..2) == Range.new(1, 2, false)")
+        assert self.unwrap(space, w_res) is True
+
+        w_res = space.execute("return (1...2) == Range.new(1, 2, true)")
+        assert self.unwrap(space, w_res) is True
+
+        w_res = space.execute("return (1..2) == (1...2)")
+        assert self.unwrap(space, w_res) is False
+
+    def test_eql_eql(self, space):
+        w_res = space.execute("return (1..2) === 1")
+        assert self.unwrap(space, w_res) is True
+
+        w_res = space.execute("return (1..2) === 3")
+        assert self.unwrap(space, w_res) is False
+
+    def test_include(self, space):
+        w_res = space.execute("return (1..5).member?(4)")
+        assert self.unwrap(space, w_res) is True
+
+        w_res = space.execute("return (1..5).member?(6)")
+        assert self.unwrap(space, w_res) is False
+
+        w_res = space.execute("return ('1'..'5').member?('2')")
+        assert self.unwrap(space, w_res) is True
+
+        w_res = space.execute("return ('a'..'f').member?('c')")
+        assert self.unwrap(space, w_res) is True
+
+    def test_cover(self, space):
+        w_res = space.execute("return (1..2).cover?(0)")
+        assert self.unwrap(space, w_res) is False
+        w_res = space.execute("return (1..2).cover?(1)")
+        assert self.unwrap(space, w_res) is True
+        w_res = space.execute("return (1..2).cover?(2)")
+        assert self.unwrap(space, w_res) is True
+        w_res = space.execute("return (1..2).cover?(3)")
+        assert self.unwrap(space, w_res) is False
+
+        w_res = space.execute("return (1...2).cover?(0)")
+        assert self.unwrap(space, w_res) is False
+        w_res = space.execute("return (1...2).cover?(1)")
+        assert self.unwrap(space, w_res) is True
+        w_res = space.execute("return (1...2).cover?(2)")
+        assert self.unwrap(space, w_res) is False
+
+    def test_allocate(self, space):
+        w_res = space.execute("return Range.allocate.begin")
+        assert self.unwrap(space, w_res) is None
+
+        w_res = space.execute("return Range.allocate.end")
+        assert self.unwrap(space, w_res) is None
+
+        w_res = space.execute("return Range.allocate.exclude_end?")
+        assert self.unwrap(space, w_res) is False
+
+    def test_new(self, space):
+        w_res = space.execute("return Range.new(1, 2).begin")
+        assert self.unwrap(space, w_res) == 1
+        w_res = space.execute("return Range.new(1, 2).end")
+        assert self.unwrap(space, w_res) == 2
+        w_res = space.execute("return Range.new(1, 2).exclude_end?")
+        assert self.unwrap(space, w_res) is False
+
+        w_res = space.execute("return Range.new(1, 2, true).begin")
+        assert self.unwrap(space, w_res) == 1
+        w_res = space.execute("return Range.new(1, 2, true).end")
+        assert self.unwrap(space, w_res) == 2
+        w_res = space.execute("return Range.new(1, 2, true).exclude_end?")
         assert self.unwrap(space, w_res) is True
